bf27286de7a078ce018390c8c3cb95aeaa0f887c
Brant <brant@Brants-MacBook-Pro.local>
2020-06-25 15:06:26 -0600

    stable at index.

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask,render_template,request
+import pandas as pd
+from bokeh.embed import components
+from bokeh.plotting import figure
+#from bkcharts import Histogram
+import numpy as np
+#from bokeh import Histogram
 
 app = Flask(__name__)
+app.vars = {}
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+      return render_template('index.html')
+    else:
+      app.vars['name'] = request.form['name_lulu']
+      app.vars['age'] = request.form['age_lulu']
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+      # Create the plot
+      data = pd.DataFrame(np.array([1, 2, 3, int(app.vars['name']), int(app.vars['age'])  ]))
+      hist, edges = np.histogram(data, density=True, bins=50)
+      plot = figure()
+      plot.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], line_color="white")
+		
+      # Embed plot into HTML via Flask Render
+      script, div = components(plot)
+      return render_template("iris_index1.html", script=script, div=div)
 
-if __name__ == '__main__':
-  app.run(port=33507)
+if __name__ == "__main__":
+    app.run(debug=True)
